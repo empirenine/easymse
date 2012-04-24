c867bb3ef53f0b5e90b6d33ee8e2d841ef74b684
Mike MacDonald <mike.macdonald@active.com>
2012-04-24 15:16:46 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Easymse::Application.config.secret_token = 'cfba8fb2ef0383e8f678170a0bc5c4a2d4d5705637a478c527257e3a01e03ea35d11f1c5b0281beeea9602fac81c26f9b5c03f8afd00da89d3ee947166a7655d'
